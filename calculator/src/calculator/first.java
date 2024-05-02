239b65b7c32324acc1dd2846ea2fc2c2078bfc73
TristanEgg <168433781+TristanEgg@users.noreply.github.com>
2024-05-02 14:37:50 +0800

    second commit

--- calculator/src/calculator/first.java
+++ calculator/src/calculator/first.java
@@ -3,51 +3,68 @@ package calculator;
 import java.util.Scanner;
 
 public class first {
-	  public static void main(String[] args) {
-		  Scanner answerObj = new Scanner(System.in);
-		  
-		  char answer = 'Y';
-		  
-		  while (answer == 'Y') {
-		  
-		  System.out.println("Calculator? (Y/N)");
-		  answer = answerObj.next().charAt(0);  
-		  
-		  if (answer == 'Y' ) {
-			  
-			  //input two numbers
-			  Scanner no1Obj = new Scanner(System.in);
-			  Scanner no2Obj = new Scanner(System.in);
-			  System.out.println("Please input first numbers?");
-			  float no1 = no1Obj.nextFloat();
-			  System.out.println("Please input second numbers?");
-			  float no2 = no1Obj.nextFloat();
-			  System.out.println("Your input is " + no1 + " and " + no2);
-			  
-			  //input operator
-			  Scanner operatorObj = new Scanner(System.in);
-			  System.out.println("Which operator? (+ - * /)");
-			  char operator = operatorObj.next().charAt(0);
-			  System.out.println("Your input is " + operator);
-			  
-			  if (operator == '+') {
-				  System.out.println(no1 + " + " + no2 + " = " + (no1 + no2) );
-			  }
-			  else if (operator == '-') {
-				  System.out.println(no1 + " - " + no2 + " = " + (no1 - no2) );
-			  }
-			  else if (operator == '*') {
-				  System.out.println(no1 + " * " + no2 + " = " + (no1 * no2) );
-			  }
-			  else if (operator == '/') {
-				  System.out.println(no1 + " / " + no2 + " = " + (no1 / no2) );
-			  }
-			  
-		  else if (answer == 'N' ) {
-			  System.out.println("Thanks you!");
-			  System. exit(0);
-			  }
-		  }
+	public static void main(String[] args) {
+		Scanner calculatorObj = new Scanner(System.in);
+		Scanner num1Obj = new Scanner(System.in);
+		Scanner num2Obj = new Scanner(System.in);
+		Scanner operatorObj = new Scanner(System.in);
+
+		String calculator = "Y";
+		float num1 = 0;
+		float num2 = 0;
+		boolean checkoperator = true;
+
+		while (calculator == "Y") {
+			System.out.println("Calculator? (Y/N)");
+			calculator = calculatorObj.nextLine();
+			if (calculator.equals("Y")) {
+				while (true) {
+					try {
+						System.out.println("Please input first numbers.");
+						num1 = num1Obj.nextFloat();
+						break;
+					} catch (Exception e) {
+						System.out.println("Please try again!");
+						num1Obj.next();
+					}
+				}
+				while (true) {
+					try {
+						System.out.println("Please input second numbers.");
+						num2 = num2Obj.nextFloat();
+						break;
+					} catch (Exception e) {
+						System.out.println("Please try again!");
+						num2Obj.next();
+					}
+				}
+
+				while (checkoperator = true) {
+					System.out.println("Which operator? (+ - * /)");
+					char operator = operatorObj.next().charAt(0);
+					if (operator == '+') {
+						System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+					} else if (operator == '-') {
+						System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+					} else if (operator == '*') {
+						System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+					} else if (operator == '/') {
+						System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+					} else {
+						checkoperator = false;
+					}
+				}
+
+			} else if (calculator.equals("N")) {
+				System.out.println("Thanks you!");
+				System.exit(0);
+			} else {
+				System.out.println("Please try again!");
+				calculator = "Y";
+			}
+
+		}
+
 	}
-		  }
-	  }
+}
+
